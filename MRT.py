3a8dbb5519d4809cf3e117e1271d3a41fd0f2b3d
ravirejo <jrr.raghuvir@gmail.com>
2017-03-31 16:29:41 -0400

    after NEBB and speed update

--- MRT.py
+++ MRT.py
@@ -5,9 +5,11 @@ use sudo apt-get install python-matplotlib
 sudo apt-get install python-vtk?
 
 '''
+#@profile
 
 from numpy import *
-import numexpr as ne
+import numpy as np
+#import numexpr as ne
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot
@@ -15,14 +17,14 @@ from VTKWrapper import saveToVTK
 import os
 from timeit import default_timer as timer
 tstart = timer()
-#from rexec import RHooks
-os.system("taskset -p 0xff %d" % os.getpid())
+
+#os.system("taskset -p 0xff %d" % os.getpid())
 
 #print('number of cores detected is'); ne.detect_number_of_threads()
-ne.set_num_threads(ne.detect_number_of_threads())
+#ne.set_num_threads(ne.detect_number_of_threads())
 
 # Plot Settings
-Pinterval = 100# iterations to print next output
+Pinterval = 1000# iterations to print next output
 SavePlot = True #save plots
 SaveVTK = True # save files in vtk format
 project = 'ldc'  #for naming output files 
@@ -31,25 +33,36 @@ CurrentFolder = os.getcwd()
 
 # Lattice Parameters
 maxIt = 200000 # time iterations
-Re    = 1000.0 # Reynolds number
+Re    = 3200.0 # Reynolds number 100 400 1000 3200 5000 7500 10000
 
 #Number of cells
-xsize, ysize = 600, 600
+xsize, ysize = 200, 200
 xsize_max, ysize_max = xsize-1, ysize-1 # highest index in each direction
 q = 9 # d2Q9
 
 uLB = 0.08 # velocity in lattice units
 nuLB = uLB*ysize/Re #viscosity coefficient
 
+
+
+
 #SRT
 omega = 2.0 / (6.*nuLB+1)
 
+#TRT
+omegap = omega  
+delTRT = 1.0/12.0 
+omegam = 1.0/(0.5 + ( delTRT/( (1/omegap)-0.5 )) )
+
 #MRT - relaxation time vector
-omega_nu = 2.0 / (6.*nuLB+1) # from shear viscosity
+omega_nu = omega # from shear viscosity
 omega_e =  1.0 # stokes hypothesis - bulk viscosity is zero
-omega_eps , omega_q = 0.72,0.72 #0.71, 0.83 # randomly chosen
-omega_vec = [1.0, omega_e, omega_eps, 1.0, omega_q, 1.0, omega_q, omega_nu, omega_nu]
+omega_eps , omega_q = 1.0,1.2 #0.71, 0.83 # randomly chosen
+omega_vec = [0.0, omega_e, omega_eps, 0.0, omega_q, 0.0, omega_q, omega_nu, omega_nu]
+omega_vec = ones((q)); omega_vec[:] = omega
 omega_diag = diag(omega_vec)
+
+
 
 ## Plot Setup
 
@@ -83,7 +96,7 @@ Ux_GhiaVer = GhiaData[:,Re_dict[Re]]
 Uy_GhiaHor = GhiaData[:,Re_dict[Re]+9]
 
 NormErr_column = []; time_column = [] # initializing arrays to track LBM and Ghia differences
-
+LBMy = array(Y_GhiaVer*ysize_max).astype(int) # LBM coordinates close to ghia's values
 #     Ux_GhiaVertAxis = GhiaRefUx_100()
 #     Uy_GhiaVertAxis = GhiaRefUy_100()
 #     Ux_GhiaVertAxis = GhiaRefUx_400()
@@ -107,8 +120,8 @@ t = 1.0/36. * ones(q)
 t[1:5] = 1.0/9.0
 t[0] = 4.0/9.0 
 
-# reverse index for no-slip bounceback condition
-noslip = [0,3,4,1,2,7,8,5,6]
+# reverse index for bounceback condition
+bounce = [0,3,4,1,2,7,8,5,6]
 
 #indexed arrays for stencil sides
 LeftStencil   = arange(q)[asarray([temp[0] <  0 for temp in c])]
@@ -143,18 +156,47 @@ M_GS_INV[8,:] = [1.0/9, 1.0/18, 1.0/36, 1.0/6, 1.0/12, -1.0/6, -1.0/12, 0, -1.0/
 
 #compute density
 def sumf(fin):
-    #return sum(fin, axis =0)
-     return ne.evaluate('sum(fin, axis =0)') # using numexpr for multithreading
+    return sum(fin, axis =0)
+    #return ne.evaluate('sum(fin, axis =0)') # using numexpr for multithreading
 #compute equilibrium distribution
+
+cu = zeros((q,xsize,ysize))
+u = zeros((2,xsize,ysize))
+u1 = zeros((2,xsize,ysize))
+fplus = zeros((q,xsize,ysize))
+fminus = zeros((q,xsize,ysize))
+feplus = zeros((q,xsize,ysize))
+feminus = zeros((q,xsize,ysize))
+
 def equ(rho,u):
-    cu   = 3.0 * dot(c, u.transpose(1, 0, 2))
-    #usqr = 3./2.*(u[0]**2+u[1]**2)
-    temp1 = ne.evaluate('u**2'); temp2 = ne.evaluate('sum(temp1,0)'); usqr = ne.evaluate('(3.0/2.0)*temp2')
+    #cu   = dot(c, u.transpose(1, 0, 2))
+    # peeling loop to increase speed
+    cu[0] = (c[0,0]*u[0] + c[0,1]*u[1])
+    cu[1] = (c[1,0]*u[0] + c[1,1]*u[1])
+    cu[2] = (c[2,0]*u[0] + c[2,1]*u[1])
+    cu[3] = (c[3,0]*u[0] + c[3,1]*u[1])
+    cu[4] = (c[4,0]*u[0] + c[4,1]*u[1])
+    cu[5] = (c[5,0]*u[0] + c[5,1]*u[1])
+    cu[6] = (c[6,0]*u[0] + c[6,1]*u[1])
+    cu[7] = (c[7,0]*u[0] + c[7,1]*u[1])
+    cu[8] = (c[8,0]*u[0] + c[8,1]*u[1])
+       
+    usqr = (u[0]*u[0]+u[1]*u[1])
+    #temp1 = ne.evaluate('u**2'); temp2 = ne.evaluate('sum(temp1,0)'); usqr = ne.evaluate('(3.0/2.0)*temp2')
     feq = zeros((q, xsize, ysize))
     for i in range(q):
-        feq[i, :, :] = rho*t[i]*(1. + cu[i] + 0.5*cu[i]**2 - usqr)
+        feq[i, :, :] = rho*t[i]*(1. + 3.0*cu[i] + 9*0.5*cu[i]*cu[i] - 3.0*0.5*usqr)
     return feq
-
+#     feq[0, :, :] = rho*t[0]*(1. + 3.0*cu[0] + 9*0.5*cu[0]*cu[0] - 3.0*0.5*usqr)
+#     feq[1, :, :] = rho*t[1]*(1. + 3.0*cu[1] + 9*0.5*cu[1]*cu[1] - 3.0*0.5*usqr)
+#     feq[2, :, :] = rho*t[2]*(1. + 3.0*cu[2] + 9*0.5*cu[2]*cu[2] - 3.0*0.5*usqr)
+#     feq[3, :, :] = rho*t[3]*(1. + 3.0*cu[3] + 9*0.5*cu[3]*cu[3] - 3.0*0.5*usqr)
+#     feq[4, :, :] = rho*t[4]*(1. + 3.0*cu[4] + 9*0.5*cu[4]*cu[4] - 3.0*0.5*usqr)
+#     feq[5, :, :] = rho*t[5]*(1. + 3.0*cu[5] + 9*0.5*cu[5]*cu[5] - 3.0*0.5*usqr)
+#     feq[6, :, :] = rho*t[6]*(1. + 3.0*cu[6] + 9*0.5*cu[6]*cu[6] - 3.0*0.5*usqr)
+#     feq[7, :, :] = rho*t[7]*(1. + 3.0*cu[7] + 9*0.5*cu[7]*cu[7] - 3.0*0.5*usqr)
+#     feq[8, :, :] = rho*t[8]*(1. + 3.0*cu[8] + 9*0.5*cu[8]*cu[8] - 3.0*0.5*usqr)
+#    return feq
 # Set up
 LeftWall = fromfunction(lambda x,y:x==0,(xsize,ysize))
 RightWall = fromfunction(lambda x,y:x==xsize_max,(xsize,ysize))
@@ -168,7 +210,9 @@ InitVel[0,:,0] = uLB
 
 # initial distributions
 feq = equ(1.0, InitVel)
+
 fin = feq.copy()
+fin1 = feq.copy()
 fpost = feq.copy()
 
 # interactive figure mode
@@ -178,21 +222,46 @@ if (SavePlot):
 
 os.chdir(OutputFolder)
 
+tmethod = ' - TRT' # options : SRT, TRT, MRT
 
 # Time Loop
 for It in range(maxIt):
     # macro density
     ftemp = fin
-    
+#     ftemp1 = fin1
+
     #TRT
-    fplus = 0.5*(fin[:,:,:] + fin[noslip[:], :,:])
-    fminus = 0.5*(fin[:,:,:] - fin[noslip[:], :,:])
-          
-    # macro velocity
+    #fplus = 0.5*(fin[:,:,:] + fin[bounce[:], :,:])
+    #fminus = 0.5*(fin[:,:,:] - fin[bounce[:], :,:])
+    #simplifying to increase speed
+#     fplus[TopStencil]  = 0.5*(fin[TopStencil] + fin[BotStencil]); fplus[BotStencil] = fplus[TopStencil]
+#     fplus[1]  = 0.5*(fin[1] + fin[3]); fplus[3] = fplus[1];fplus[0] = fin[0]
+#     fminus[TopStencil]  = 0.5*(fin[TopStencil] - fin[BotStencil]); fminus[BotStencil] = -fminus[TopStencil]
+#     fminus[1]  = 0.5*(fin[1] - fin[3]); fminus[3] = -fminus[1];fminus[0] = 0
+    
+    fplus[2]  = 0.5*(fin[2] + fin[4]); fplus[4] = fplus[2]
+    fplus[5]  = 0.5*(fin[5] + fin[7]); fplus[7] = fplus[5]
+    fplus[6]  = 0.5*(fin[6] + fin[8]); fplus[8] = fplus[6]
+    fplus[1]  = 0.5*(fin[1] + fin[3]); fplus[3] = fplus[1];fplus[0] = fin[0]
+    fminus[2]  = 0.5*(fin[2] - fin[4]); fminus[4] = -fminus[2]
+    fminus[5]  = 0.5*(fin[5] - fin[7]); fminus[7] = -fminus[5]
+    fminus[6]  = 0.5*(fin[6] - fin[8]); fminus[8] = -fminus[6]
+    fminus[1]  = 0.5*(fin[1] - fin[3]); fminus[3] = -fminus[1];fminus[0] = 0  
+    
     
     #print(It)
     rho = sumf(fin)
-    u = dot(c.transpose(), fin.transpose((1,0,2)))/rho
+#     rho1 = sumf(fin1)
+    #u = dot(c.transpose(), fin.transpose((1,0,2)))/rho
+    #peeling the loop to increase the speed
+    u[0,:,:] = (c[0,0]*fin[0]+c[1,0]*fin[1]+c[2,0]*fin[2]+c[3,0]*fin[3]+c[4,0]*fin[4]+c[5,0]*fin[5]+c[6,0]*fin[6]+c[7,0]*fin[7]+c[8,0]*fin[8])/rho
+    u[1,:,:] = (c[0,1]*fin[0]+c[1,1]*fin[1]+c[2,1]*fin[2]+c[3,1]*fin[3]+c[4,1]*fin[4]+c[5,1]*fin[5]+c[6,1]*fin[6]+c[7,1]*fin[7]+c[8,1]*fin[8])/rho
+
+#     u1[0,:,:] = (c[0,0]*fin1[0]+c[1,0]*fin1[1]+c[2,0]*fin1[2]+c[3,0]*fin1[3]+c[4,0]*fin1[4]+c[5,0]*fin1[5]+c[6,0]*fin1[6]+c[7,0]*fin1[7]+c[8,0]*fin1[8])/rho
+#     u1[1,:,:] = (c[0,1]*fin1[0]+c[1,1]*fin1[1]+c[2,1]*fin1[2]+c[3,1]*fin1[3]+c[4,1]*fin1[4]+c[5,1]*fin1[5]+c[6,1]*fin1[6]+c[7,1]*fin1[7]+c[8,1]*fin1[8])/rho
+
+
+#     
    #u = zeros((2,xsize,ysize))
    #for m in range(xsize):
         #for l in range(2):
@@ -200,43 +269,60 @@ for It in range(maxIt):
            
     
     rho[:, 0] = sumf(ftemp[CentHStencil, :, 0])+2.*sumf(ftemp[TopStencil, :, 0])
-    #u[:,:,0]=InitVel[:,:,0]
-    
+#     rho1[:, 0] = sumf(ftemp1[CentHStencil, :, 0])+2.*sumf(ftemp1[TopStencil, :, 0])
+ #   u[:,:,0]=InitVel[:,:,0]
+    u[:,0,1:]= 0 ; u[:,xsize_max,1:]= 0 ; u[:,:,ysize_max]= 0
+    u[0,10:xsize_max-10,0]=uLB ; u[1,10:xsize_max-10,0] =0 #10 chosen randomly to not force corners
     feq = equ(rho,u)
+#     feq1 = equ(rho1,u1)
     
     #MRT
 #     m_GS = dot(M_GS,fin.transpose(1,0,2))  # warning : m_GS is not same as M_GS
-#      
-#     m_GS_eq = m_GS # initiating m_GS equilibrium
-#     m_GS_eq[0,:,:] = rho    
-#     m_GS_eq[1,:,:] = rho*(-2.0 + 3.0*(u[0,:,:]*u[0,:,:] + u[1,:,:]*u[1,:,:]))
-#     m_GS_eq[2,:,:] =  - 3.0*rho*(u[0,:,:]*u[0,:,:] + u[1,:,:]*u[1,:,:]) + rho
-#      
-#     m_GS_eq[3,:,:] = rho*u[0,:,:]
-#      
-#     m_GS_eq[4,:,:] = - rho*u[0,:,:]
-#     m_GS_eq[5,:,:] = rho*u[1,:,:]
-#     m_GS_eq[6,:,:] = - rho*u[1,:,:]
-#     m_GS_eq[7,:,:] = rho*(u[0,:,:]*u[0,:,:] - u[1,:,:]*u[1,:,:])
-#     m_GS_eq[8,:,:] = rho*u[0,:,:]*u[1,:,:]
-    #TRT
-    feplus = 0.5*(feq[:,:,:] + feq[noslip[:], :,:])
-    feminus = 0.5*(feq[:,:,:] - feq[noslip[:], :,:])
+#     jx = m_GS[3]; jy = m_GS[5]
+#     m_GS_eq = m_GS.copy() # initiating m_GS equilibrium
+#     jx = m_GS[3]; jy = m_GS[5] 
+#     m_GS_eq[1,:,:] = -2.0*rho + 3.0*(jx*jx + jy*jy)
+#     m_GS_eq[2,:,:] =  - 3.0*(jx*jx + jy*jy) + rho
+#     m_GS_eq[4,:,:] = - jx
+#     m_GS_eq[6,:,:] = - jy
+#     m_GS_eq[7,:,:] = jx*jx - jy*jy
+#     m_GS_eq[8,:,:] = jx*jy 
+
+    #TRT    
+    #feplus = 0.5*(feq[:,:,:] + feq[bounce[:], :,:])
+    #feminus = 0.5*(feq[:,:,:] - feq[bounce[:], :,:])
+    #simplifying to increase speed
+#     feplus[TopStencil]  = 0.5*(feq[TopStencil] + feq[BotStencil]); feplus[BotStencil] = feplus[TopStencil]
+#     feplus[1]  = 0.5*(feq[1] + feq[3]); feplus[3] = feplus[1];feplus[0] = feq[0]
+#     feminus[TopStencil]  = 0.5*(feq[TopStencil] - feq[BotStencil]); feminus[BotStencil] = -feminus[TopStencil]
+#     feminus[1]  = 0.5*(feq[1] - feq[3]); feminus[3] = -feminus[1];feminus[0] = 0    
+   
+    feplus[2]  = 0.5*(feq[2] + feq[4]); feplus[4] = feplus[2]
+    feplus[5]  = 0.5*(feq[5] + feq[7]); feplus[7] = feplus[5]
+    feplus[6]  = 0.5*(feq[6] + feq[8]); feplus[8] = feplus[6]
+    feplus[1]  = 0.5*(feq[1] + feq[3]); feplus[3] = feplus[1];feplus[0] = feq[0]
+    feminus[2]  = 0.5*(feq[2] - feq[4]); feminus[4] = -feminus[2]
+    feminus[5]  = 0.5*(feq[5] - feq[7]); feminus[7] = -feminus[5]
+    feminus[6]  = 0.5*(feq[6] - feq[8]); feminus[8] = -feminus[6]
+    feminus[1]  = 0.5*(feq[1] - feq[3]); feminus[3] = -feminus[1];feminus[0] = 0 
     
-    #Collision - MRT
+    #Collision - MRT    
+#     m_GS = m_GS - dot(omega_diag, transpose((m_GS-m_GS_eq), (1,0,2)))
+#     fpost = dot(M_GS_INV , m_GS.transpose(1,0,2))
+#     temp = dot(omega_diag, transpose((m_GS-m_GS_eq), (1,0,2)))
+#     fpost = fin - dot(M_GS_INV , transpose(temp, (1,0,2)) ) 
     
-#    m_GS = m_GS - dot(omega_diag, transpose((m_GS-m_GS_eq), (1,0,2)))
-#    fpost = dot(M_GS_INV , m_GS.transpose(1,0,2))
-    
+       
     #TRT
     #Collision - TRT
-    omegap, omegam = 0.98, 1.2
     fpost = fin - omegap*(fplus-feplus) - omegam*( fminus-feminus)
         
     #Collision - SRT
     #fpost = fin - omega*( fin-feq)    
-   
     
+
+
+    #print(mean(fpost))
 
     #streaming
     
@@ -252,47 +338,77 @@ for It in range(maxIt):
     fin[7, 0:xsize_max-1, 1:ysize_max]   = fpost[7, 1:xsize_max,   0:ysize_max-1]
     fin[8, 1:xsize_max,   1:ysize_max]   = fpost[8, 0:xsize_max-1, 0:ysize_max-1]
     
+#     fin1[0, :, :] = fpost1[0, :, :]
+#      
+#     fin1[1, 1:xsize_max,   :]     = fpost1[1, 0:xsize_max-1,  :]
+#     fin1[2,   :,   0:ysize_max-1] = fpost1[2,   :,    1:ysize_max]
+#     fin1[3, 0:xsize_max-1, :]     = fpost1[3, 1:xsize_max,    :]
+#     fin1[4,   :,   1:ysize_max]   = fpost1[4,   :,    0:ysize_max-1]
+#  
+#     fin1[5, 1:xsize_max,   0:ysize_max-1] = fpost1[5, 0:xsize_max-1, 1:ysize_max]
+#     fin1[6, 0:xsize_max-1, 0:ysize_max-1] = fpost1[6, 1:xsize_max,   1:ysize_max]
+#     fin1[7, 0:xsize_max-1, 1:ysize_max]   = fpost1[7, 1:xsize_max,   0:ysize_max-1]
+#     fin1[8, 1:xsize_max,   1:ysize_max]   = fpost1[8, 0:xsize_max-1, 0:ysize_max-1]
      
+ 
     #Accounting for moving wall using zou-he condition
     # boundary condition at walls
     
- 
-  
-    for value in LeftStencil: fin[value, RightWall ] = ftemp[noslip[value], RightWall] 
-    for value in TopStencil: fin[value, BottomWall ] = ftemp[noslip[value], BottomWall]  
-    for value in RightStencil: fin[value, LeftWall ] = ftemp[noslip[value], LeftWall]  
-#     fin[LeftStencil, RightWall ] = ftemp[asarray( [noslip[i] for i in LeftStencil]) , RightWall]
-#     fin[TopStencil, BottomWall ] = ftemp[asarray( [noslip[i] for i in TopStencil]) , BottomWall]
-#     fin[RightStencil, LeftWall ] = ftemp[asarray( [noslip[i] for i in RightStencil]) , LeftWall]
-    
-    #fin[BotStencil, 0, :] = - feq[TopStencil, 0, :] + (feq[BotStencil, 0, :] + ftemp[TopStencil, 0, :])
 
-    temp = uLB
-    uLB = 0
+#Simple Bounceback 
+#     for value in LeftStencil: fin[value, RightWall ] = ftemp[bounce[value], RightWall] 
+#     for value in TopStencil: fin[value, BottomWall ] = ftemp[bounce[value], BottomWall]  
+#     for value in RightStencil: fin[value, LeftWall ] = ftemp[bounce[value], LeftWall]  
 
-    fin[4,0,:] = fin[2,0,:]
-    fin[7,0,:] = fin[5,0,:] + 0.5*(fin[1,0,:] - fin[3,0,:]) - 0.5*uLB
-    fin[8,0,:] = fin[6,0,:] - 0.5*(fin[1,0,:] - fin[3,0,:]) + 0.5*uLB
-     
-    #corners - upper left and then upper right
+
+
+#     fin[LeftStencil, RightWall ] = ftemp[asarray( [bounce[i] for i in LeftStencil]) , RightWall]
+#     fin[TopStencil, BottomWall ] = ftemp[asarray( [bounce[i] for i in TopStencil]) , BottomWall]
+#     fin[RightStencil, LeftWall ] = ftemp[asarray( [bounce[i] for i in RightStencil]) , LeftWall]
     
+    # NEBB for walls
+    
+    fin[RightStencil, 0, :] = - feq[LeftStencil, 0, :] + (feq[RightStencil, 0, :] + ftemp[LeftStencil, 0, :])
+    fin[TopStencil, :, ysize_max] = - feq[BotStencil, :, ysize_max] + (feq[TopStencil, :, ysize_max] + ftemp[BotStencil, :, ysize_max])
+    fin[LeftStencil, xsize_max, :] = - feq[RightStencil, xsize_max, :] + (feq[LeftStencil, xsize_max, :] + ftemp[RightStencil, xsize_max, :])
+    fin[BotStencil, :, 0] = - feq[TopStencil, :, 0] + (feq[BotStencil, :, 0] + ftemp[TopStencil, :, 0])
+    
+#     fin1[RightStencil, 0, :] = - feq1[LeftStencil, 0, :] + (feq1[RightStencil, 0, :] + ftemp1[LeftStencil, 0, :])
+#     fin1[TopStencil, :, ysize_max] = - feq1[BotStencil, :, ysize_max] + (feq1[TopStencil, :, ysize_max] + ftemp1[BotStencil, :, ysize_max])
+#     fin1[LeftStencil, xsize_max, :] = - feq1[RightStencil, xsize_max, :] + (feq1[LeftStencil, xsize_max, :] + ftemp1[RightStencil, xsize_max, :])
+#     fin1[BotStencil, :, 0] = - feq1[TopStencil, :, 0] + (feq1[BotStencil, :, 0] + ftemp1[TopStencil, :, 0])
+#        
+
+#NEBB accounting for tangential velocities
+#     temp = uLB
+#     uLB = 0
+#    
+#     fin[4,:,0] = fin[2,0,:]
+#     fin[7,:,0] = fin[5,0,:] + 0.5*(fin[1,:,0] - fin[3,:,0]) - 0.5*uLB
+#     fin[8,:,0] = fin[6,0,:] - 0.5*(fin[1,:,0] - fin[3,:,0]) + 0.5*uLB
+#        
+# #     #corners - upper left and then upper right
+#  #   uLB = 0     
     fin[1,0,0] = fin[3,0,0] + (2.0/3.0)*uLB
     fin[4,0,0] = fin[2,0,0] 
     fin[8,0,0] = fin[6,0,0] + (1.0/6.0)*uLB
     fin[5,0,0] = +(1.0/12.0)*uLB
     fin[7,0,0] = -(1.0/12.0)*uLB
     fin[0,0,0] = 1.0 - sumf(fin[1:,0,0])
-    fin[3,0,xsize_max] = fin[1,0,xsize_max] - (2.0/3.0)*uLB
-    fin[4,0,xsize_max] = fin[2,0,xsize_max] 
-    fin[7,0,xsize_max] = fin[5,0,xsize_max] - (1.0/6.0)*uLB
-    fin[6,0,xsize_max] =  -(1.0/12.0)*uLB
-    fin[8,0,xsize_max] =  +(1.0/12.0)*uLB
-    fin[0,0,xsize_max] = 1.0 - sumf(fin[1:,0,xsize_max])    
-    uLB = temp
+    fin[3,xsize_max,0] = fin[1,xsize_max,0] - (2.0/3.0)*uLB
+    fin[4,xsize_max,0] = fin[2,xsize_max,0] 
+    fin[7,xsize_max,0] = fin[5,xsize_max,0] - (1.0/6.0)*uLB
+    fin[6,xsize_max,0] =  -(1.0/12.0)*uLB
+    fin[8,xsize_max,0] =  +(1.0/12.0)*uLB
+    fin[0,xsize_max,0] = 1.0 - sumf(fin[1:,xsize_max,0])    
+#     uLB = temp
      
+    
     if( (It%Pinterval == 0) & (SaveVTK | SavePlot)) :
        
         print ('current iteration :', It)
+        print (mean(u[0,:,0]/uLB))
+        print (mean(u[1,0,:]/uLB))
         if (SavePlot):
             
             f.clear()
@@ -328,22 +444,25 @@ for It in range(maxIt):
             subplot3.set_xlabel('X-position', fontsize = 20);subplot3.set_ylabel('Y-position', fontsize = 20)
             
             #print((YNorm == array(Y_GhiaVer*ysize_max).astype(int)))
-            Ux_temp = u[0,int(xsize/2),array(Y_GhiaVer*ysize_max).astype(int)]
-            NormErr_column.append(1 - sumf(square(flip(Ux_GhiaVer,0)- Ux_temp))/(len(Ux_GhiaVer)*var(Ux_GhiaVer)) ) #rsquare estimate
+            Ux_temp = u[0,int(xsize/2),LBMy]/uLB
+            NormErr_column.append(1 - sumf(square(fliplr(atleast_2d(Ux_GhiaVer))[0] - Ux_temp))/(len(Ux_GhiaVer)*var(Ux_GhiaVer)) ) #rsquare estimate
+            
+            #NormErr_column.append(1 - sumf(square(fliplr(atleast_2d(Ux_GhiaVer))[0] - Ux_temp))/sumf(square(Ux_GhiaVer)) ) #rsquare estimate
+
             time_column.append(It)
             subplot4.plot(time_column,NormErr_column,)
             subplot4.set_title('Normalized difference between LBM and Ghia Data')  
             subplot4.set_xlabel('time iteration', fontsize = 20);subplot4.set_ylabel('Normalized difference', fontsize = 20)
 
-            f.suptitle('Lid Driven Cavity - Re' + str(int(Re)), fontsize = 30, y =1.04)
-            pyplot.savefig(project + "." + str(It/Pinterval).zfill(4) + ".png",bbox_inches = 'tight', pad_inches = 0.4)
+            f.suptitle('Lid Driven Cavity - Re' + str(int(Re)) + tmethod, fontsize = 30, y =1.04)
+            pyplot.savefig(project + "_" + str(int(It/Pinterval)).zfill(5) + ".png",bbox_inches = 'tight', pad_inches = 0.4)
 
         if ( SaveVTK ):
             # convert 2d data to 3d arrays
             Vel = reshape(u, (2, xsize, ysize, 1))
             Vel3D = (Vel[0, :, :, :], Vel[1, :, :, :], velZ)   
             Rho3D = reshape(rho, (xsize, ysize, 1))
-            index = str(It/Pinterval).zfill(4)
+            index = str(int(It/Pinterval)).zfill(5)
             saveToVTK(Vel3D, Rho3D, project, index, grid)
         tend = timer()
 
@@ -353,11 +472,11 @@ for It in range(maxIt):
 os.chdir(CurrentFolder)
 tend = timer()
 
-print ( 'time elapsed is ', (tend-tstart), 'seconds' )
+print ( 'TOTAL time elapsed is ', (tend-tstart), 'seconds' )
 
-def main():
-    print('the end')
+#def main():
+#    print('the end')
 
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+ #   main()
